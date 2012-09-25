fc280032df7991527ababc37ab975def2880dd04
root <root@testsrv.mos.com.np>
2012-09-25 16:33:57 +0545

    one blog created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f86c7fe065d5b4d9e49bc3673817d190be68a6ec7e3883e57e8ab0ba93d8e32f3b258db7a7950334ce2ae7b96169a1ba3aa91c1f41c84c8950048a4630d18e77'
